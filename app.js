61404b3728b60f0cae6de1d5b00fe04591e17f2a
Himanshu Negi <himanshunegi267@gmail.com>
2025-08-12 14:50:32 +0530

    CORS setup

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const tokenizerService = require("./services/TokenService");
+const cors=require('cors');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,7 +10,7 @@ app.use(bodyParser.json());
 connectDB().then(async () => {
     await tokenizerService.init(); 
 });
-
+app.use(cors());
 // Routes
 const tokenizerRoutes = require("./routes/routes.js");
 app.use("/api/tokenizer", tokenizerRoutes);
